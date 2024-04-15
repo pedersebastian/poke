2e650ebcb5465adc44e4bac3c2213f20079c3fd2
pedersebastian <86198089+pedersebastian@users.noreply.github.com>
2024-04-15 19:23:23 +0200

    Create loop_all.py

---
+++ loop_all.py
@@ -0,0 +1,29 @@
+from funs import pokevenn
+import pandas as pd
+import os, PIL
+from tqdm import tqdm
+
+
+
+def main():
+    res = []
+    paths = os.listdir("bilder")
+
+    for file in tqdm(paths):
+        try:
+            poke = pokevenn(f"bilder/{file}")
+            res.append(poke)
+        except:
+            print(f"{file} funka ikke")
+            
+            
+
+    df = pd.DataFrame(res)
+
+
+    df.to_csv("pokevenner2.csv")
+    
+    
+if __name__ == "__main__":
+    main()
+
